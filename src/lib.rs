f69b91a214292417dcb9b3a4187d2ff83463cdba
Warhorst <46809804+Warhorst@users.noreply.github.com>
2023-11-27 17:01:33 +0100

    Created a plugin that loads all assets and switches to a given state when done.
    Added bevy_ecs, bevy_asset and bevy_app as dependencies.

---
+++ src/lib.rs
@@ -0,0 +1,81 @@
+use bevy_app::prelude::*;
+use bevy_asset::{LoadedFolder, RecursiveDependencyLoadState};
+use bevy_asset::prelude::*;
+use bevy_ecs::prelude::*;
+
+/// Plugin that starts loading all assets in the asset folder for a given state and
+/// automatically switches to another given state when everything is loaded.
+pub struct AssetPreloadPlugin<LoadingState: States, NextState: States> {
+    /// The state the plugin will start and keep loading all assets.
+    loading_state: LoadingState,
+    /// The state the plugin will switch to when all assets are loaded
+    next_state: NextState
+}
+
+/// Resource that holds handles to all assets in the assets folder. This only exists to ensure
+/// the assets don't get unloaded because nobody is using them.
+#[derive(Resource)]
+struct LoadedAssets(Vec<UntypedHandle>);
+
+/// Resource that holds the handle to the currently loading asset folder.
+#[derive(Resource)]
+struct LoadingAssetFolder(Handle<LoadedFolder>);
+
+impl LoadingAssetFolder {
+    /// Tells if the asset folder is loaded. Might panic if the load failed.
+    fn is_loaded(&self, asset_server: &AssetServer) -> bool {
+        match asset_server.recursive_dependency_load_state(&self.0) {
+            RecursiveDependencyLoadState::Failed => panic!("some assets failed loading, abort"),
+            RecursiveDependencyLoadState::Loaded => true,
+            _ => false
+        }
+    }
+}
+
+impl<LoadingState: States, NextState: States> Plugin for AssetPreloadPlugin<LoadingState, NextState> {
+    fn build(&self, app: &mut App) {
+        app
+            .add_systems(
+                OnEnter(self.loading_state.clone()),
+                start_asset_loading
+            )
+            .add_systems(
+                Update,
+                switch_state_when_all_loaded(self.next_state.clone()).run_if(in_state(self.loading_state.clone()))
+            )
+        ;
+    }
+}
+
+/// Start loading the assets folder, and store the folder handle in a resource.
+fn start_asset_loading(
+    mut commands: Commands,
+    asset_server: Res<AssetServer>
+) {
+    let folder_handle = asset_server.load_folder("./");
+    commands.insert_resource(LoadingAssetFolder(folder_handle));
+}
+
+/// Create a system that will check if the assets folder was loaded. If true, the system will switch to the
+/// provided state and deletes the folder handle. Every handle from the folder will be preserved in a LoadedAssets resource.
+fn switch_state_when_all_loaded<S: States>(followup_state: S) -> impl Fn(Commands, Res<AssetServer>, Res<LoadingAssetFolder>, Res<Assets<LoadedFolder>>, ResMut<NextState<S>>) {
+    move |mut commands, asset_server, loading_asset_folder, loaded_folders, mut next_state| {
+        if !loading_asset_folder.is_loaded(&asset_server) {
+            return
+        }
+
+        let folder = loaded_folders.get(loading_asset_folder.0.id()).expect("the folder should be loaded");
+
+        let loaded_assets = LoadedAssets(
+            folder.handles
+                .iter()
+                .map(Clone::clone)
+                .collect()
+        );
+
+        commands.insert_resource(loaded_assets);
+        commands.remove_resource::<LoadingAssetFolder>();
+
+        next_state.set(followup_state.clone())
+    }
+}
